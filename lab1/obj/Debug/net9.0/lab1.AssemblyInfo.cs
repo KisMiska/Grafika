d4700c662f2d96dc6fb2688fd0f74e2d4c6fd31a
KisMiska <galicaervin@gmail.com>
2025-03-19 22:50:58 +0200

    lab1-2 bonusz

--- lab1/obj/Debug/net9.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net9.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9175ddb7f6800c94a09321a1dec651bed80fb0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2095df9a0e155d3a1ea29a00447a17920ef54e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
