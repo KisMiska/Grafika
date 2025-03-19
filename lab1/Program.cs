8ef6182b12e25355fca2783045984a1905735c49
KisMiska <galicaervin@gmail.com>
2025-03-19 17:47:14 +0200

    colorArray elrontva

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -110,13 +110,13 @@ namespace Szeminarium1
             float[] vertexArray = new float[] {
                 -0.5f, -0.5f, 0.0f,
                 +0.5f, -0.5f, 0.0f,
-                 0.0f, +0.5f, /*0.0f,*/
+                 0.0f, +0.5f, 0.0f,
                  1f, 1f, 0f
             };
 
             float[] colorArray = new float[] {
                 1.0f, 0.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, /*0.0f,*/ 1.0f,
                 0.0f, 0.0f, 1.0f, 1.0f,
                 1.0f, 0.0f, 0.0f, 1.0f,
             };
